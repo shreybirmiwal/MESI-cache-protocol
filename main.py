0f3cfff8a72e4705bd7a1cd76cf6b8b9f90259be
Shrey Birmiwal <shreybirmiwal@gmail.com>
2025-12-14 18:19:00 -0600

    thinking

---
+++ main.py
@@ -0,0 +1,15 @@
+objects_in_memory = {"0x100":"A", "0x104":"B", "0x108":"C"}
+cores = {"Core 1": [["0x100", "A", "M"], ["0x104", "F", "I"]], 
+         "Core 2": [["0x108", "C", "E"]]}
+
+
+def addObjectToMemory():
+    # a
+
+def read():
+    # if core cache get it
+    # if dirty do work
+    # get from main memory if needed
+    
+
+def write():
